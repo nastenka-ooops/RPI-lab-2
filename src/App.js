b1f1030c30c6e894e871c7b5a66d16453f5b9d8a
nastenka-ooops <nastenka.brutskaya@mail.ru>
2023-12-16 15:04:38 +0300

    проект

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Button} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <Button variant="primary">Button</Button>
+        </div>
+    );
+}
+
+export default App;
